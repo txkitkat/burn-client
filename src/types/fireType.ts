fbfe4301cf2f4a4af4a3c571263ace4b298265bc
smk-dev20 <smk.dev20@gmail.com>
2021-10-15 15:30:14 -0700

    fetch and display data from backend

---
+++ src/types/fireType.ts
@@ -0,0 +1,12 @@
+export default interface IFire {
+    year: number | any,
+    date: string | any,
+    name: string | any,
+    acres: number,
+    latitude: number,
+    longitude: number,
+    county: string | any,
+    source: string | any,
+    burnType: string | any
+  }
+  
