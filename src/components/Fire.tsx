134b901202286aec466166b8391827400469ccdc
smk-dev20 <smk.dev20@gmail.com>
2021-10-24 21:07:38 -0700

    changing marker color back to blue

--- src/components/Fire.tsx
+++ src/components/Fire.tsx
@@ -2,9 +2,8 @@ import {Circle, Popup} from "react-leaflet";
 
 function Fire(props: any) {
     let date = props.date.substring(0, props.date.indexOf("T"));
-    const fillRed = { color: 'red' }
     return (
-        <Circle center={props.latLon} radius={props.burnArea} pathOptions={fillRed}>
+        <Circle center={props.latLon} radius={props.burnArea} >
             <Popup>
                 <div>
                     Fire with name: {props.name} burned on date: {date} and was size: {props.burnArea} acres.
