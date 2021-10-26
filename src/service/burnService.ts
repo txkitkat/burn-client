950e600f2e185b7c41f70eeb1126434ece6597fe
smk-dev20 <smk.dev20@gmail.com>
2021-10-25 17:34:41 -0700

    process.env host update

--- src/service/burnService.ts
+++ src/service/burnService.ts
@@ -1,46 +1,50 @@
 import http from "../http-common";
 
 class BurnService {
+
+     host = process.env.REACT_APP_FIRE_BACKEND;
+
     getAll(param: any[]) {
         // @ts-ignore
-        return http.get(`/fires`);
+        console.log(this.host+`/fires`);
+        return http.get(this.host+`/fires`);
     }
 
     getBySource(source : any[]){
-        return http.get(`/fires/search/findBySource?source=${source[0]}`);
+        return http.get(this.host+`/fires/search/findBySource?source=${source[0]}`);
 
     }
 
     getByCounty(county : any[]){
-        return http.get(`/fires/search/findByCounty?county=${county[0]}`);
+        return http.get(this.host+`/fires/search/findByCounty?county=${county[0]}`);
     }
 
     getByBurntype(type : any[]){
-        return http.get(`/fires/search/findByBurnType?burnType=${type[0]}`);
+        return http.get(this.host+`/fires/search/findByBurnType?burnType=${type[0]}`);
 
     }
 
     getByYear(year : any[]){
-        return http.get(`/fires/search/findByYear?year=${year[0]}`);
+        return http.get(this.host+`/fires/search/findByYear?year=${year[0]}`);
 
     }
 
     getByRangeOfYears(years: any[]){
-        return http.get(`/fires/search/findByYearIsBetween?fromYear=${years[0]}&toYear=${years[1]}`)
+        return http.get(this.host+`/fires/search/findByYearIsBetween?fromYear=${years[0]}&toYear=${years[1]}`)
     }
 
     getByAcres(acres : any[]){
-        return http.get(`/fires/search/findByAcres?acres=${acres[0]}`);
+        return http.get(this.host+`/fires/search/findByAcres?acres=${acres[0]}`);
 
     }
 
     getByRangeOfAcres(acres : any[]){
-        return http.get(`/fires/search/findByAcresIsBetween?min=${acres[0]}&max=${acres[1]}`);
+        return http.get(this.host+`/fires/search/findByAcresIsBetween?min=${acres[0]}&max=${acres[1]}`);
 
     }
 
     getByDates(dates : any[]){
-        return http.get(`/fires/search/findByDateIsBetween?fromDate=${dates[0]}&toDate=${dates[1]}`);
+        return http.get(this.host+`/fires/search/findByDateIsBetween?fromDate=${dates[0]}&toDate=${dates[1]}`);
 
     }
 }
