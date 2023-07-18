68927262e6c0647672e8cb2cfd8f10a3916b8ebd
AjCH1 <78681938+AjCH1@users.noreply.github.com>
2023-07-17 23:18:47 -0700

    Update MapLayerPickerControl
    Show legend and map in same option.

--- src/components/Map/MapLayerPickerControl.tsx
+++ src/components/Map/MapLayerPickerControl.tsx
@@ -1,4 +1,4 @@
-import { LayersControl, ImageOverlay } from "react-leaflet";
+import { LayersControl, ImageOverlay, LayerGroup } from "react-leaflet";
 import CountyBorders from "../CountyBorders"
 import vegetationImage from "../../data/WHR13_vegetation.png"
 import vegetationLegend from "../../data/WHR13_vegetation_legend.png"
@@ -11,14 +11,17 @@ export default function MapLayerPickerControl(props: {seed: number}) {
                 <CountyBorders />
             </LayersControl.Overlay>
             <LayersControl.Overlay name="Vegetation Overlay">
-                <ImageOverlay url={vegetationImage} bounds={[[42.160435, -124.814949], [32.566587, -113.922004]]} opacity={0.75} />
-            </LayersControl.Overlay>
-            <LayersControl.Overlay name="Vegetation Legend">
-                <ImageOverlay url={vegetationLegend} bounds={[[43.07287, -130.92884], [38.669518, -127.03968]]} opacity={1} />
+                <LayerGroup>
+                    <ImageOverlay url={vegetationImage} bounds={[[42.160435, -124.814949], [32.566587, -113.922004]]} opacity={0.75} />
+                    <ImageOverlay url={vegetationLegend} bounds={[[43.07287, -130.92884], [38.669518, -127.03968]]} opacity={1} />
+                </LayerGroup>
             </LayersControl.Overlay>
             {(props.seed > 1) ? (
             <LayersControl.Overlay name="Burn Window">
-                <ImageOverlay url={`${process.env.REACT_APP_FIRE_WINDOW_BACKEND}/image`} bounds={[[42.005413, -124.504903], [32.529048, -114.116167]]} opacity={0.4} />
+                <LayerGroup>
+                    <ImageOverlay url={`${process.env.REACT_APP_FIRE_WINDOW_BACKEND}/image`} bounds={[[42.005413, -124.504903], [32.529048, -114.116167]]} opacity={0.4} />
+                    <ImageOverlay url={`${process.env.REACT_APP_FIRE_WINDOW_BACKEND}/legend`} bounds={[[43.07287, -130.92884], [38.669518, -127.03968]]} opacity={1} />
+                </LayerGroup>
             </LayersControl.Overlay>
             ) : (<></>)}
         </LayersControl>
