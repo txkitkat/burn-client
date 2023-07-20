d6fc35704755508b6acc814fba2c6f6950961cf8
AjCH1 <78681938+AjCH1@users.noreply.github.com>
2023-07-20 06:08:56 -0700

    Change Fire Color

--- src/components/Fire.tsx
+++ src/components/Fire.tsx
@@ -1,9 +1,21 @@
 import {Circle, Popup} from "react-leaflet";
 
+
 function Fire(props: any) {
+
+    const fireColors = {
+        baseFireColor: {
+          rgb: "rgb(255, 128, 0)",
+        },
+    };
+
     let date = (props.month+1)+'/'+props.day+'/'+props.year;
     return (
-      <Circle center={props.latLon}>
+      <Circle 
+        center={props.latLon} 
+        radius = {50}
+        color={fireColors['baseFireColor'].rgb}
+        fillColor={fireColors['baseFireColor'].rgb}>
             <Popup>
                 <div>
                     Fire with name: {props.name} burned on date: {date} in County: {props.county} and was size: {props.burnArea} acres.
